a156488e2c07aad629d7e38c6095b46f5cd2707b
Gheraldine Salazar <103468156+GheraldineSalazarL@users.noreply.github.com>
2023-02-01 18:42:29 -0300

    Desafio 3 Coder - Servidores Web

---
+++ Desafio3/src/ProductManager.js
@@ -0,0 +1,83 @@
+import { promises } from 'fs';
+
+export default class ProductManager {
+
+  constructor(ruta) {
+    this.ruta = ruta;
+  }
+
+  async save(product) {
+    const products = await this.getAll();
+        if(products.length === 0){
+            product.id = 1;
+        } else{
+            product.id =  products[products.length -1].id + 1;
+        }
+        products.push(product);
+
+        await promises.writeFile(path, JSON.stringify(products, null, '\t'));
+        return product;
+  }
+
+  async getById(id) {
+    const products = await this.getAll();
+        
+    const productIndex=products.findIndex(e=>e.id===id); 
+
+    if(productIndex === -1) { 
+        console.log("Notfound");
+        return;
+    }
+
+    return(products[productIndex]);
+  }
+
+  async getAll() {
+    try {
+      const products = await promises.readFile(this.ruta, 'utf-8');
+      return JSON.parse(products);
+    } catch (error) {
+      console.log(error);
+      return [];
+    }
+  }
+
+  async deleteById(id) {
+    const products = await this.getAll();
+    const product = await this.getById(id);
+
+    // let index = products.findIndex(index => index.id = id)
+
+    const removedProduct =  products.splice(product[Index],1)
+
+    await promises.writeFile(path, JSON.stringify(products, null, '\t'));
+    return removedProduct;    
+  }
+
+  async deleteAll() {
+    const products = await this.getAll();
+
+    products.length = products.length - products.length;
+
+    await promises.writeFile(path, JSON.stringify(products, null, '\t'));
+  }
+
+  async update(id, field, value) {
+    const products = await this.getAll();
+
+    if(field==="id") { return "error" };
+
+    const updatedProduct = 
+        products.map(prod =>
+            prod.id === id
+            ? { ...prod, 
+                    [field]: value,
+                    }
+            : prod
+        );  
+
+    await promises.writeFile(path, JSON.stringify(updatedProduct, null, '\t'));
+  }
+
+
+}
---
+++ Desafio3/src/app.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import ProductManager from './ProductManager.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
+
+const productManager = new ProductManager(path.join(dirname, 'productos.json'));
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/products', async (req, res) => {
+    const products = await productManager.getAll();
+    const {limit} = req.query;
+
+    if(!limit) return res.send(products);
+
+    const productosFiltrados = []
+    if(limit <= products.length){
+        for(var i=0; i<limit; i++){
+            productosFiltrados.push(products[i])
+        }
+        res.send(productosFiltrados)
+    } else {res.send({error:`Sólo exiten ${products.length} productos`})}
+
+    console.log(limit)
+})
+
+app.get('/products/:pid', async (req,res)=> {
+    const pid = Number(req.params.pid);
+    const product = await productManager.getById(pid);
+    if(!product) return res.send({error:'Producto no encontrado'});
+    res.send(product);
+    console.log(pid)
+})
+
+
+app.listen(8080,()=>console.log("Listening on 8080"))
